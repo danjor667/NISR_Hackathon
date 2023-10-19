019060a46cef42be619d8816646a4d0ed729d01a
danjor667 <jordannguepi@gmail.com>
2023-10-19 22:19:05 +0200

    refactoring code

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_extras.metric_cards import style_metric_cards
 from data.loading import load_season_a, load_season_b, load_season_c
 import plotly_express as px
 
-st.set_page_config(layout="wide")
+
+st.set_page_config("seasons Insight", layout="wide")
+st.subheader("2022 Agricultural seasons Insight")
+
 
 
 @st.cache_data
@@ -15,43 +20,88 @@ def load():
 season_A, season_B, season_C = load()
 
 
-st.title("insight of the 2022 agricultural season in Rwanda:bar_chart:")
-graph = st.selectbox("season", ("season A", "season B", "season C"))
-st.sidebar.header("filter")
+
+def season_a():
+    # processing and plotting the graph for seasons A
+    st.write("season A")
+
+
+
+
+def season_b():
+    # processing and plotting graph for season B data
+    st.write("season B")
+    #st.dataframe(season_B)
+    with st.sidebar:
+        pass
+
+
+def season_c():
+    # processing and plotting graphs for the season C data
+    df = season_C
+    df.set_index("District", inplace=True)
+    st.write("season C")
+    with st.sidebar:
+        st.subheader("Filter")
+        districts = st.multiselect("District", df.index)
+        crops = st.multiselect("Crops", df.columns)
+    if districts and crops:
+        # create dataframe with the selected crops and Districts
+        df = df.loc[districts, crops]
+    else:
+        # define default district and crops to plot as the whole df can't be visualised at once on the graph
+        df = df.loc[[df.index[0], df.index[1], df.index[2], df.index[3], df.index[4]], [df.columns[0], df.columns[5], df.columns[3]]]
+
+    def cards():
+        total_land = 0
+        for land in df.sum():
+           total_land += land
+
+        #st.write(df.sum())
+
+        card_1, card_2, card_3 = st.columns(3)
+        card_1.metric("Min Land Used:", value=0)
+        card_2.metric("Max Land Used:", value=0)
+        card_3.metric("Total Land used in Ha", value=total_land)
+        style_metric_cards(background_color="#6600ff")
+
+    with st.container():
+        cards()
+        div_1, div_2 = st.columns([0.45, 0.55], gap="small")
+        with div_1:
+            description_df = df.sum()
+            description_df.columns = ["crop", "total land used"]
+            #st.write(description_df.columns)
+            st.dataframe(description_df, use_container_width=True)
+            st.write("Table Description")
+        with div_2:
+            with st.expander("See Table"):
+                st.dataframe(df, height=247, use_container_width=True)
+
+
+
+    # plotting barchart and pie chart representation of data
+    bar_fig = px.bar(df, barmode="group", y=df.columns.to_list(), x=df.index.to_list())
+    # plotting a pie-chart diag for the df
+    pie_fig = px.pie(df)
+    with st.container():
+        st.plotly_chart(bar_fig, use_container_width=True)
+    with st.container():
+        st.plotly_chart(pie_fig, use_container_width=True)
+
+
 
 with st.sidebar:
-    districts = st.multiselect("Select Districts", season_A["District"].tolist())
-    crops = st.multiselect("Select Crop", season_A.columns.tolist()[1:-1], default=season_A.columns.tolist()[1:-1])
-if districts and crops:
-    new_df = season_A.loc[season_A["District"].isin(districts), crops]
+    season = option_menu("SEASONS",
+                         ["Season A", "Season B", "Season C"],
+                         menu_icon="house")
+
+if season == "Season A":
+    # call function for season A
+    season_a()
+elif season == "Season B":
+    # call function for season b
+    season_b()
 else:
-    new_df = season_A
-
-season_A_fig1 = season_A.drop("Total developed land", axis=1).drop(32).melt(id_vars='District', var_name='Crop',value_name='Land_Used')
-fig_1 = px.line(season_A_fig1, x='Crop', y='Land_Used', color='District',
-                title='Land Used vs Various Crop Grown by District SEASON A 2022')
-fig_1.update_layout(yaxis=dict(dtick=1000), height=600, width=800)
-
-season_A_fig2 = season_A.drop(32).drop("Total developed land", axis=1).melt(id_vars='District', var_name="Crop",
-                                                                            value_name='Land_used')
-fig_2 = px.pie(season_A_fig2, values='Land_used', names='Crop', color='Crop',
-               title='percentage of land used by each crop Nation-wide')
-fig_2.update_traces(textinfo=None, textposition="inside")
-
-with st.container():
-    if graph == "season A":
-        with st.expander("see DataFrame"):
-            st.dataframe(new_df, use_container_width=True)
-    col1, col2 = st.columns([0.62, 0.38], gap="medium")
-
-with col1:
-    if graph == "season A":
-        st.plotly_chart(fig_1, use_container_width=True)
-    else:
-        st.write("not yet done")
-
-with col2:
-    if graph == "season A":
-        st.plotly_chart(fig_2, use_container_width=True)
-    else:
-        st.write("not yet done")
+    # call function for season C
+    season_c()
