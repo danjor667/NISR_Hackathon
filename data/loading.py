b1cab8d525d20c0a667d6e43de2f28defd18dc6e
danjor667 <jordannguepi@gmail.com>
2023-11-29 08:18:57 +0200

    restart 1

---
+++ data/loading.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python3
+
+import streamlit as st
+import pandas as pd
+
+
+# cleaning data for the land usage section
+@st.cache_data
+def load_land_a():
+    season_a_data = pd.read_excel('data/2022_Tables.xlsx', sheet_name="Table 11")
+    season_a_data = season_a_data.drop("List of Tables", axis=1)
+    season_a_data = season_a_data.drop(0)
+    season_a_data.columns = season_a_data.iloc[0]
+    season_a_data = season_a_data[1:32]
+    season_a_data = season_a_data.fillna(0)
+    season_a_data.at[32, "District"] = "National"
+    season_a_data = season_a_data.drop("Total developed land", axis=1)
+    season_a_data.rename(columns={"Beans": "Bean"}, inplace=True)
+    return season_a_data
+
+
+# loading and cleaning season B data
+@st.cache_data
+def load_and_b():
+    season_b_data = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 12 ")  # trailing whit space in sheet name
+    season_b_data = season_b_data.drop("List of Tables", axis=1)
+    season_b_data = season_b_data.drop(0)
+    season_b_data.columns = season_b_data.iloc[0]
+    season_b_data = season_b_data[1:32]
+    season_b_data = season_b_data.fillna(0)
+    season_b_data.rename(columns={"District/Crop category": "District"}, inplace=True)
+    season_b_data.at[32, "District"] = "National"
+    season_b_data = season_b_data.drop("Total developed land", axis=1)
+    season_b_data.rename(columns={"Bean": "Beans"}, inplace=True)
+    season_b_data.rename(columns={"Chia seed": "Chia seeds"}, inplace=True)
+    season_b_data.rename(columns={"Sweet potato": "Sweet potatoes"}, inplace=True)
+    season_b_data.rename(columns={"Irish potato": "Irish potatoes"}, inplace=True)
+    season_b_data.rename(columns={"Taro & Yams": "Yams & Taro"}, inplace=True)
+    season_b_data.rename(columns={"Banana": "Bananas"}, inplace=True)
+    season_b_data.rename(columns={"Cooking banana": "Cooking Banana"}, inplace=True)
+    season_b_data.rename(columns={"Pea": "Peas"}, inplace=True)
+    season_b_data.rename(columns={"Groundnut": "Ground nuts"}, inplace=True)
+    season_b_data.rename(columns={"Soybean": "Soya beans"}, inplace=True)
+    season_b_data.rename(columns={"Other cereals": "Other Cereals"}, inplace=True)
+    return season_b_data
+
+
+# loading and cleaning season C data
+@st.cache_data
+def load_land_c():
+    season_c_data = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 13")
+    season_c_data = season_c_data.drop(columns=["List of Tables", "Unnamed: 1"], axis=1)
+    season_c_data = season_c_data.drop(0)
+    season_c_data.columns = season_c_data.iloc[0]
+    season_c_data = season_c_data[1:32]
+    season_c_data = season_c_data.fillna(0)
+    season_c_data.rename(columns={"District/Crop": "District"}, inplace=True)
+    season_c_data.rename(columns={"Sweet potato": "Sweet potatoes"}, inplace=True)
+    season_c_data.rename(columns={"Irish potato": "Irish potatoes"}, inplace=True)
+    season_c_data.rename(columns={"Beans": "Bean"}, inplace=True)
+    season_c_data.rename(columns={"Pea": "Peas"}, inplace=True)
+    season_c_data.rename(columns={"Soybean": "Soya beans"}, inplace=True)
+    season_c_data.rename(columns={"vegetables": "Vegetables"}, inplace=True)
+    season_c_data.at[32, "District"] = "National"
+    season_c_data = season_c_data.drop("Developed land", axis=1)
+    return season_c_data
+
+
+# cleaning data for the crop production section
+
+def load_crop_production_a():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 23")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df.columns = df.iloc[0]
+    df = df[1:33]
+    df = df.fillna(0)
+    df.rename(columns={"Vegetables": "vegetables"}, inplace=True)
+    df.rename(columns={"Other Cereals": "Other cereals"}, inplace=True)
+    df.rename(columns={"Cooking Banana": "Cooking banana"}, inplace=True)
+    return df
+
+
+def load_crop_production_b():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 24")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df = df.drop(1)
+    df = df.drop(2)
+    df.columns = df.iloc[0]
+    df = df[1:33]
+    df = df.fillna(0)
+    return df
+
+
+def load_crop_production_c():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 25")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df = df.drop(1)
+    df.columns = df.iloc[0]
+    df = df.drop(df.columns[0], axis=1)
+    df = df[1:33]
+    df = df.fillna(0)
+    df.rename(columns={" Bean": "Bean"}, inplace=True)
+    df.rename(columns={"District/Crop": "District"}, inplace=True)
+    return df
+
+
+# cleaning data for the crop yield
+def load_crop_yield_a():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 17")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df.columns = df.iloc[0]
+    df = df[1:32]
+    df = df.fillna(0)
+    df.rename(columns={"District/Crop category": "District"}, inplace=True)
+    return df
+
+
+def load_crop_yield_b():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 18 ")  # trailing spaceat the end of the sheet name
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df = df.drop(1)
+    df.columns = df.iloc[0]
+    df = df[1:32]
+    df = df.fillna(0)
+    df.rename(columns={"District/Crop category": "District"}, inplace=True)
+    return df
+
+
+def load_crop_yield_c():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 19")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df.columns = df.iloc[0]
+    df = df.drop(df.columns[0], axis=1)
+    df = df[1:32]
+    df = df.fillna(0)
+    df.rename(columns={"District/Crop": "District"}, inplace=True)
+    return df
+
+
+def load_harvested_a():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 14")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df.columns = df.iloc[0]
+    df = df[1:32]
+    df = df.fillna(0)
+    df.rename(columns={"District/Crop category": "District"}, inplace=True)
+    return df
+
+
+def load_harvested_b():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 15 ") #trailing white space
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 14")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df.columns = df.iloc[0]
+    df = df[1:32]
+    df = df.fillna(0)
+    df.rename(columns={"District/Crop category": "District"}, inplace=True)
+    return df
+
+
+def load_harvested_c():
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 16")
+    df = pd.read_excel("data/2022_Tables.xlsx", sheet_name="Table 14")
+    df = df.drop("List of Tables", axis=1)
+    df = df.drop(0)
+    df.columns = df.iloc[0]
+    df = df[1:32]
+    df = df.fillna(0)
+    df.rename(columns={"District/Crop category": "District"}, inplace=True)
+    return df
---
+++ main.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python3
+import pandas as pd
+import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_extras.metric_cards import style_metric_cards
+from data.loading import *
+import plotly_express as px
+
+
+st.set_page_config("Agricultural land usage", layout="centered")
+st.subheader("2022 Agricultural season land usage in Rwanda")
+st.divider()
+
+
+@st.cache_data
+def load_land():
+    return load_land_a(), load_and_b(), load_land_c()
+
+
+@st.cache_data
+def load_production():
+    return load_crop_production_a(), load_crop_production_b(), load_crop_production_c()
+
+
+@st.cache_data
+def load_yield():
+    return load_crop_yield_a(), load_crop_yield_b(), load_crop_yield_c()
+
+
+land_a_df, land_b_df, land_c_df = load_land()
+production_a_df, production_b_df, production_c_df = load_production()
+yield_a_df, yield_b_df, yield_c_df = load_yield()
+
+
+
+@st.cache_data
+def processing(land_df, production_df, yield_df, season_name):
+    st.write(season_name)
+
+    def cards():
+        max_land = land_df.max().max()
+        max_land_district = land_df[land_df == max_land].stack().index[0][1]
+        max_land_crop = land_df[land_df == land_df.max().max()].stack().index[0][0]
+        ####
+        max_production = production_df.max().max()
+        max_pro_district = production_df[production_df == max_production].stack().index[0][1]
+        max_pro_crop = production_df[production_df == production_df.max().max()].stack().index[0][0]
+        ###
+        max_yield = yield_df.max().max()
+        max_yield_district = yield_df[yield_df == max_yield].stack().index[0][1]
+        max_yield_crop = yield_df[yield_df == yield_df.max().max()].stack().index[0][0]
+        #st.write(max_land_crop)
+        card_1, card_2, card_3, card_4 = st.columns(4)
+        card_1.metric("Top Land Usage (Hectars):", value=f"{max_land:,.2f}",
+                      delta=f"{max_land_district}  {max_land_crop.upper()}")
+        card_2.metric("Top Production (in MT)", value=f"{max_production:,.2f}",
+                      delta=f"{max_pro_district}  {max_pro_crop.upper()}")
+        card_3.metric("Top Average Yield (in kg/Ha)", value=f"{max_yield:,.2f}",
+                      delta=f"{max_yield_district}  {max_yield_crop.upper()}")
+        card_4.metric("Total land used Nation Wide", value=0, delta="Total")
+        style_metric_cards(background_color="#696865")
+
+
+    # with st.container():
+    cards()
+    st.write("")
+    st.write("")
+    st.write("")
+    # plotting barchart and pie chart representation of data
+    land_fig = px.bar(land_df, barmode="group", y=land_df.columns.to_list(), x=land_df.index.to_list(),
+                     title="land used vs crop grown by Districts")
+    land_fig.update_layout(xaxis_title="CROP", yaxis_title="LAND USED (in ha)")
+    land_fig.update_layout(yaxis=dict(dtick=250), height=600)
+    # processing and plotting the graph for the creop production
+    production_fig = px.bar(production_df, barmode="group", y=production_df.columns.to_list(),
+                            x=production_df.index.to_list(), title="various crop production by district")
+    # processing and plotting the garphfor the the yield
+
+    yield_fig = px.bar(yield_df, barmode="group", y=yield_df.columns.to_list(), x=yield_df.index.to_list(),
+                      title="Average crop yield by district(kg/Ha)")
+
+
+
+    tab1, tab2, tab3 = st.tabs([":bar_chart: Land Usage(in hectars)", ":bar_chart: Crop production(in MegaTonne)",
+                                ":bar_chart: Average crop yield by district(kg/Ha)"])
+    with tab1:
+        with st.expander("see Table"):
+            st.dataframe(land_df, use_container_width=True)
+        st.plotly_chart(land_fig, use_container_width=True)
+        st.caption("                    testing caption")
+    with tab2:
+        with st.expander("see Table"):
+            st.dataframe(production_df, use_container_width=True)
+        st.plotly_chart(production_fig, use_container_width=True)
+    with tab3:
+        with st.expander("See Table"):
+            st.dataframe(yield_df, use_container_width=True)
+        st.plotly_chart(yield_fig, use_container_width=True)
+
+    st.divider()
+
+
+with st.sidebar:
+    season = option_menu("SEASONS",
+                         ["Season A", "Season B", "Season C"],
+                         menu_icon="house")
+
+
+if season == "Season A":
+    land_df = land_a_df
+    production_df = production_a_df
+    production_df.drop(32)
+    production_df.drop(33)
+    yield_df = yield_a_df
+    name = "Season A"
+    land_df = land_df.drop(32)
+    land_df.set_index("District", inplace=True)
+    production_df.set_index("District", inplace=True)
+    yield_df.set_index("District", inplace=True)
+    with st.sidebar:
+        st.subheader("Filter")
+        districts = st.multiselect("District", land_df.index, default=[land_df.index[0], land_df.index[1], land_df.index[2],
+                                                                       land_df.index[3], land_df.index[4]])
+        crops = st.multiselect("Crops", land_df.columns, default=[land_df.columns[0], land_df.columns[1], land_df.columns[2]])
+    if districts and crops:
+        # create dataframe with the selected crops and Districts
+        land_df = land_df.loc[districts, crops]
+        production_df = production_df.loc[districts, crops]
+        yield_df = yield_df.loc[districts, crops]
+    processing(land_df, production_df, yield_df, name)
+elif season == "Season B":
+    land_df = land_b_df
+    production_df = production_b_df
+    production_df.drop(32)
+    production_df.drop(33)
+    yield_df = yield_b_df
+    name = "Season B"
+    land_df = land_df.drop(32)
+    land_df.set_index("District", inplace=True)
+    production_df.set_index("District", inplace=True)
+    yield_df.set_index("District", inplace=True)
+    with st.sidebar:
+        st.subheader("Filter")
+        districts = st.multiselect("District", land_df.index,
+                                   default=[land_df.index[0], land_df.index[1], land_df.index[2],
+                                            land_df.index[3], land_df.index[4]])
+        crops = st.multiselect("Crops", land_df.columns,
+                               default=[land_df.columns[0], land_df.columns[1], land_df.columns[2]])
+    if districts and crops:
+        # create dataframe with the selected crops and Districts
+        land_df = land_df.loc[districts, crops]
+        production_df = production_df.loc[districts, crops]
+        yield_df = yield_df.loc[districts, crops]
+    processing(land_df, production_df, yield_df, name)
+
+else:
+    land_df = land_c_df
+    production_df = production_c_df
+    production_df.drop(32)
+    production_df.drop(33)
+    yield_df = yield_c_df
+    name = "Season C"
+    land_df = land_df.drop(32)
+    land_df.set_index("District", inplace=True)
+    production_df.set_index("District", inplace=True)
+    yield_df.set_index("District", inplace=True)
+    with st.sidebar:
+        st.subheader("Filter")
+        districts = st.multiselect("District", land_df.index,
+                                   default=[land_df.index[0], land_df.index[1], land_df.index[5],
+                                            land_df.index[3], land_df.index[4]])
+        crops = st.multiselect("Crops", land_df.columns,
+                               default=[land_df.columns[0], land_df.columns[1], land_df.columns[3]])
+    if districts and crops:
+        # create dataframe with the selected crops and Districts
+        land_df = land_df.loc[districts, crops]
+        production_df = production_df.loc[districts, crops]
+        yield_df = yield_df.loc[districts, crops]
+    processing(land_df, production_df, yield_df, name)
+
+hide_default_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden}
+                </style>
+                """
+st.markdown(hide_default_style, unsafe_allow_html=True)
